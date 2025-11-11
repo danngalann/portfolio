91327144b252d0511df412ff91915da7525a1973
Daniel <dgalan@delectatech.com>
2025-11-11 09:46:48 +0100

    Add a navbar to the desktop view

---
+++ app/ui/navbar.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import { useInView } from "react-intersection-observer";
+
+export default function Navbar() {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    initialInView: true,
+  });
+
+  return (
+    <>
+      {/* Sentinel element at the very top - not sticky */}
+      <div ref={ref} className="h-0" />
+
+      {/* Sticky navbar */}
+      <nav
+        className={`sticky top-0 z-50 transition-all duration-300 ${
+          !inView ? "bg-foreground/70 backdrop-blur-md text-background" : ""
+        }`}
+      >
+        <ul className="flex gap-8 p-6 justify-center">
+          <li>
+            <Link href="#experience-section" className="relative group">
+              <span className="relative">
+                Experience
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-current transition-all duration-300 group-hover:w-full"></span>
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#projects-section" className="relative group">
+              <span className="relative">
+                Projects
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-current transition-all duration-300 group-hover:w-full"></span>
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#about" className="relative group">
+              <span className="relative">
+                About me
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-current transition-all duration-300 group-hover:w-full"></span>
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
